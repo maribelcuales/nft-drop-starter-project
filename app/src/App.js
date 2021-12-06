9a890610e85fbb9e5620f7ec577f2a6986d1051f
maribelcuales <maribelcuales@gmail.com>
2021-12-06 21:06:43 +0800

    Added a function to the Solana object to check if user has given us permission to use their wallet on our site.

--- app/src/App.js
+++ app/src/App.js
@@ -20,6 +20,15 @@ const App = () => {
       if (solana) {
         if (solana.isPhantom) {
           console.log('Phantom wallet found!');
+
+          /* 
+           The Solana object gives us a function that will allow us to connect directly with the user's wallet 
+          */
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            'Connected with Public Key:', 
+            response.publicKey.toString()
+          );
         }
       } else {
         alert('Solana object not found! Get a Phantom Wallet 👻');
@@ -40,7 +49,7 @@ const App = () => {
     window.addEventListener('load', onLoad);
     return () => window.removeEventListener('load', onLoad);
   }, []);
-  
+
   return (
     <div className="App">
       <div className="container">
