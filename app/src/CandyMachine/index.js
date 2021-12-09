dba6eb9e866a215d19353942d6bc95a3e60c13a3
maribelcuales <maribelcuales@gmail.com>
2021-12-09 22:16:36 +0800

    Created getMachineState async method.

--- app/src/CandyMachine/index.js
+++ app/src/CandyMachine/index.js
@@ -86,8 +86,42 @@ const CandyMachine = ({ walletAddress }) => {
       window.solana,
       opts.preflightCommitment
     );
-    
+
     return provider;
+  };
+
+  const getCandyMachineState = async () => {
+    const provider = getProvider();
+
+    // Get metadata about your deployed candy machine program 
+    const idl = await Program.fetchIdl(candyMachineProgram, provider);
+
+    // Create a program that you can call 
+    const program = new Program(idl, candyMachineProgram, provider); 
+
+    // Fetch the metadata from your candy machine 
+    const candyMachine = await program.account.candyMachine.fetch(
+      process.env.REACT_APP_CANDY_MACHINE_ID
+    );
+
+    // Parse out all our metadata and log it out 
+    const itemsAvailable = candyMachine.data.itemsAvailable.toNumber();
+    const itemsRedeemed = candyMachine.itemsRedeemed.toNumber();
+    const itemsRemaining = itemsAvailable - itemsRedeemed;
+    const goLiveData = candyMachine.data.goLiveData.toNumber();
+
+    // We will be using this later in our UI so let's generate this now 
+    const goLiveDateTimeString = `${new Date(
+      goLiveData * 1000
+    ).toGMTString()}`
+
+    console.log({
+      itemsAvailable,
+      itemsRedeemed,
+      itemsRemaining,
+      goLiveData,
+      goLiveDateTimeString,
+    });
   };
 
   const getMetadata = async (mint) => {
