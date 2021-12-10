c96a049912dbb2ae56e6d6f51211bb18ffa7534e
maribelcuales <maribelcuales@gmail.com>
2021-12-10 22:47:34 +0800

    Set mint button onclick to null for now.

--- app/src/CandyMachine/index.js
+++ app/src/CandyMachine/index.js
@@ -321,7 +321,7 @@ const CandyMachine = ({ walletAddress }) => {
       <div className="machine-container">
         <p>{`Drop Date: ${machineStats.goLiveDateTimeString}`}</p>
         <p>{`Items Minted: ${machineStats.itemsRedeemed} / ${machineStats.itemsAvailable}`}</p>
-        <button className="cta-button mint-button" onClick={mintToken}>
+        <button className="cta-button mint-button" onClick={null}>
           Mint NFT
         </button>
       </div>
